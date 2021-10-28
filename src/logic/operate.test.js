29084d5cca81c7d31e6b595fe4155b13e2aaf867
KatIsCoding <fabrigoru@gmail.com>
2021-10-28 13:50:23 -0600

    Linters Fix

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,4 +1,3 @@
-import Big from 'big.js';
 import operate from './operate';
 
 it('makes basic math operations', () => {
@@ -10,29 +9,29 @@ it('makes basic math operations', () => {
   const by = '÷';
   const modulo = '%';
   const sum = operate(a, b, plus);
-  expect(sum).toBe(`${a+b}`);
+  expect(sum).toBe(`${a + b}`);
   const sub = operate(a, b, minus);
-  expect(sub).toBe(`${a-b}`);
+  expect(sub).toBe(`${a - b}`);
   const mult = operate(a, b, times);
-  expect(mult).toBe(`${a*b}`);
+  expect(mult).toBe(`${a * b}`);
   const division = parseFloat(operate(a, b, by));
-  expect(division).toBe(parseFloat(a/b));
+  expect(division).toBe(parseFloat(a / b));
   const mod = operate(a, b, modulo);
-  expect(mod).toBe(`${a%b}`);
-})
+  expect(mod).toBe(`${a % b}`);
+});
 
-it('will crash if there is a division by 0', () =>{
+it('will crash if there is a division by 0', () => {
   const a = Math.floor(Math.random() * 10);
   const b = Math.floor(Math.random() * 10);
   const by = '÷';
   const crash = operate(a, b, by);
   if (a === 0) {
     expect(crash).toBe('can\'t divide by zero');
-  }else {
+  } else {
     const division = parseFloat(operate(a, b, by));
-    expect(division).toBe(parseFloat(a/b));
+    expect(division).toBe(parseFloat(a / b));
   }
   if (b === 0) {
     expect(crash).toBe('can\'t divide by zero');
   }
-})
+});
